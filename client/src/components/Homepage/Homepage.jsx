9eea6bc1b42a328cd81784e126786daeeb0ed75d
Dmitry Borisenko <brdima92@gmail.com>
2023-03-09 22:57:49 +0300

    add layout

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Grid, Segment } from 'semantic-ui-react';
+
+import LeftMenu from '../LeftMenu/LeftMenu';
 
 export default function Homepage() {
   return (
-    <div>Homepage</div>
+    <Grid
+      columns={2}
+      celled
+      style={{
+        height: '90vh',
+      }}
+    >
+      <Grid.Row>
+        <Grid.Column
+          width={3}
+        >
+          <Segment>
+            <LeftMenu />
+          </Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment>1</Segment>
+          <Segment>2</Segment>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+
   );
 }
